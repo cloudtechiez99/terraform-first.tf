687fa5e27ce306301ccd05af25eeb48de0696c66
cloudtechiez99 <119155403+cloudtechiez99@users.noreply.github.com>
2023-05-16 19:47:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,29 @@
-provider "google" {
-  credentials = file("creds.json")
-  project = "{{YOUR GCP PROJECT}}"
-  region  = "us-central1"
-  zone    = "us-central1-c"
-}
-
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "e2-micro"
+  name         = var.resource_name
+  machine_type = var.resource_machine_name
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = var.resource_image
     }
   }
 
   network_interface {
     # A default network is created for all GCP projects
-    network = "default"
+    network = var.network_type
     access_config {
     }
   }
+}
+
+
+resource "google_compute_disk" "custom_disk" {
+  name  = var.disk_name
+  type  = var.disk_type
+  zone  = var.zone_name
+  image = var.resource_image
+  labels = {
+    environment = var.disk_label
+  }
+  physical_block_size_bytes = var.disk_size
 }
